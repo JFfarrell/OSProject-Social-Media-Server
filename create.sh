1e3d650f57415b93e52ea808d96114a6aa9e6aa2
Jonathan <jonathan.farrell2@ucdconnect.ie>
2020-12-22 17:47:43 +0000

    testing

--- create.sh
+++ create.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-
+echo "$1"
 if [ "$#" -ne 1 ]; then
-    echo "exit code 1"
-elif [ -d "$#" ]; then
-    echo "name already taken"
+    echo "Error: Number of arguments must be 1."
+elif [ -d "$1" ]; then
+    echo "Error: Name already taken."
 else
+   ./P.sh "$0"
     mkdir "$1"
+    touch "$1"/wall
+    touch "$1"/friends
+    echo "User created successfully."
+    ./V.sh "$0"
+    exit 0
 fi
