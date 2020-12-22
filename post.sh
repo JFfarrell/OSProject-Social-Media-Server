8cae5db65c101df5e953649c42cd9e5bef06d738
Jonathan <jonathan.farrell2@ucdconnect.ie>
2020-12-22 19:40:24 +0000

    error check post.sh

--- post.sh
+++ post.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-receiver="$1"
-sender="$2"
-post="$3"
-
 if [ "$#" -ne 3 ]; then
     echo "Error: This function requires three arguments.">&2
     exit 1
 
-elif [ ! -d "$receiver" ]; then
+elif [ ! -d "$1" ]; then
     echo "Error: $receiver does not exist.">&2
     exit 1
-elif [ ! -d "$sender" ]; then
+elif [ ! -d "$2" ]; then
     echo "Error: $sender does not exist.">&2
     exit 1
 fi
 
-./P.sh "$receiver/wall"
-if grep -Fxq "$sender" "$receiver/friends"; then
-    echo "$receiver":"$post" >> "$receiver/wall"
+./P.sh "$1/wall"
+if grep -Fxq "$2" "$1/friends"; then
+    echo "$2":"$3" >> "$1/wall"
     echo "Ok: Message posted to $receiver's wall."
     ./V.sh "$receiver/wall"
     exit 0
 
 else
-    echo "$receiver is not friends with $sender."
-    ./V.sh "$receiver/wall"
+    echo "$1 is not friends with $sender."
+    ./V.sh "$1/wall"
     exit 1
 fi
 
