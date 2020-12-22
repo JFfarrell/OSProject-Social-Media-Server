38244bdc804a38c1b1484b96ba223f42144f0cfc
Jonathan <jonathan.farrell2@ucdconnect.ie>
2020-12-22 18:05:31 +0000

    checking server.sh and show.sh

--- server.sh
+++ server.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+serverpipe="/home/cs16205776/assignment/project/server"
+mkfifo "$serverpipe"
+
+while true; do
+    read arg1 arg2 arg3 arg4 < "$serverpipe";
+
+    case "$arg1" in
+        create)
+            ./create.sh "$arg2"&
+            ;;
+        add)
+            ./add.sh "$arg2" "$arg3"&
+            ;;
+        post)
+            ./post.sh "$arg2" "$arg3" "$arg4"&
+            ;;
+        show)
+            ./show.sh "$arg2"&
+            ;;
+        shutdown)
+            if [ "$arg2" == "exit" ]; then
+                echo "Shutting down..."
+                exit 0
+                break
+            fi
+            ;;
+    esac
+done
--- show.sh
+++ show.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Must provide only one argument." >&2
+    exit 1
+
+elif [ ! -d "$1" ]; then
+    echo "Error: User does not exist.">&2
+    exit 1
+
+else
+    ./P.sh "$0"
+    echo "wallStart"
+    cat "$1"/wall
+    echo "wallEnd"
+    ./V.sh "$0"
+    exit 0
+fi
